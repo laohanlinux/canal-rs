d23aba041394a06fa38a7090133903703f2066c4
huarong.zheng <huarong.zheng@geekymedic.cn>
2019-11-25 22:12:11 +0800

    update

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,30 @@
-pub struct Client {
+use std::net::{SocketAddr};
+use async_std::prelude::*;
+use async_std::io;
+use async_std::net::TcpStream;
+use async_std::task;
+use log::{info, trace, warn};
 
+pub struct Client {
+    addr: SocketAddr,
 }
 
 impl Client{
+    pub fn new(addr: SocketAddr) -> Client {
+        Client{
+            addr
+        }
+    }
+
+    pub fn connect(&mut self) -> Result<(), String> {
+        task::block_on(async {
+            let mut stream = TcpStream::connect(self.addr).await.unwrap();
+
+        });
+
+        Ok(())
+    }
+
     pub fn get_with_out_ack(batch_size: usize, timeout: Option<i64>, uints: Option<i32>) {
 
     }
